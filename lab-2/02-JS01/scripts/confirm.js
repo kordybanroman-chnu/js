f9578179871bc11768b84efc6022379b4b9a4372
Kordyban Roman <kordyban.roman@chnu.edu.ua>
2025-09-24 11:54:28 +0300

    2

---
+++ lab-2/02-JS01/scripts/confirm.js
@@ -0,0 +1,30 @@
+/*
+Метод confirm дозволяє вивести діалогове вікно з повідомленням і двома кнопками –
+Оk і Відміна (Cancel). На відміну від методу alert цей метод повертає логічну величину,
+значення якої залежить від того, на якій з двох кнопок клацнув користувач. Якщо він
+клацнув на кнопці Оk, то повертається значення true (істина, так); якщо він клацнув на
+кнопці Скасування, то повертається значення false:
+*/
+
+var r = confirm("Готові практикувати JavaScript?");
+if (r == true) {
+  myTxt.innerHTML = "Супер! Продовжуємо навчання!";
+} else {
+  myTxt.innerHTML = "Не засмучуйтеся, спробуйте ще раз!";
+}
+var myImage = document.querySelector('img');
+var j = 0;
+myImage.onclick = function () {
+  j++;
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/firefox-icon_bw.png');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+  if (j == 3) {
+    myImage.remove();
+    document.write("<h1>Вітаю!</h1><h2>Завдання виконано!</h2>");
+  }
+}
+myTxt.innerHTML += "<br>" + "  Клацніть мишею по емблемі FireFox!";
